6fbce22bb9348635abeb72e05598fc1cb2250f5a
Nidhal Anis BOUZARA <nidhalanis.bouzara@gmail.com>
2021-12-06 20:40:11 +0100

    Made old messages load on scroll.

--- controllers/MessageController.ts
+++ controllers/MessageController.ts
@@ -5,7 +5,8 @@ import { Message, User } from ".prisma/client";
 import { reverse } from "lodash";
 import { getSession } from "@utils/Auth/simpleAuth";
 
-export const getMessagesFromDB = async (cursor: number | false = false) => {
+export type MessageWithUser = Message & { user: { username: string } }
+export const getMessagesFromDB = async (cursor: number | false = false): Promise<MessageWithUser[]> => {
   const messages = reverse(await prisma.message.findMany({
     take: 100,
     skip: cursor ? 1 : 0,
@@ -18,15 +19,14 @@ export const getMessagesFromDB = async (cursor: number | false = false) => {
   return messages
 }
 
-export type GET_MESSAGE_RES = { messages: Message & Pick<User, 'username'>, cursor: number }
+export type GET_MESSAGE_RES = { messages: MessageWithUser[], new_cursor: number }
 const getMessages = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     let cursor: number | false
-    if ( req.query.params ) cursor = Number(req.query.params[0])
+    if ( req.query.params ) cursor = Number(req.query.params[0]); 
     else cursor = false
     const messages = await getMessagesFromDB(cursor)
-    return res.json({ messages, cursor })
-
+    return res.json({ messages, new_cursor: messages[0].id })
   } catch (err) {
     return handleError(res, 422, (err as Error).message)
   }
--- pages/chat.tsx
+++ pages/chat.tsx
@@ -1,36 +1,56 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useCallback } from 'react'
 import faker from 'faker'
 
 // styles
 import sheet from '@styles/pages/chat.module.scss'
 import { useForm } from 'react-hook-form'
-import { GetServerSideProps, GetServerSidePropsContext, GetServerSidePropsResult, NextPage } from 'next'
+import { GetServerSideProps, GetServerSidePropsContext, NextPage } from 'next'
 import Axios from 'axios'
 import { User } from '.prisma/client'
-import _, { reverse } from 'lodash'
-import { getMessages, getMessagesFromDB, POST_MESSAGE_RES } from '@controllers/MessageController'
+import _ from 'lodash'
+import { getMessagesFromDB, GET_MESSAGE_RES, MessageWithUser, POST_MESSAGE_RES } from '@controllers/MessageController'
 import { DateTime } from 'luxon'
 import { toast } from 'react-toastify'
 import { Message } from '@prisma/client'
 import { getSession, SessionUser } from '@utils/Auth/simpleAuth'
 import { makeSerializable } from '@utils/common'
 
-type MessageWithUser = Message & { user: Pick<User, 'username'> }
 type FormValues = Partial<MessageWithUser>
 
 interface Props {
   user: SessionUser
   messages: MessageWithUser[]
-  cursor?: number
+  cursor: number
 }
 
 const Chat: NextPage<Props> = ({ user, messages, cursor }) => {
   const { register, reset, setValue, formState: { isSubmitting }, handleSubmit } = useForm<FormValues>({ defaultValues:{ message: '' } })
   const [Messages, setMessages] = useState<MessageWithUser[]>([])
+  const [GettingMessages, setGettingMessages] = useState<boolean>(false)
+  const [Cursor, setCursor] = useState<number>(cursor)
+  const [LoadedAllMessages, setLoadedAllMessages] = useState<boolean>(false)
 
   useEffect(() => {
     setMessages(messages)
   }, [messages])
+
+  useEffect(() => {
+    if (!GettingMessages) return
+    if (Cursor <= 1) {setLoadedAllMessages(true); return;}
+    const ScrollContainer = document.getElementById("messages_scroller") as HTMLDivElement
+    const current_scroll = Number((ScrollContainer).scrollTop)
+    Axios.get(`/api/message/${Cursor}`)
+      .then(({ data }) => {
+        const { messages: new_messages, new_cursor } = data as GET_MESSAGE_RES
+        setMessages([...new_messages, ...Messages])
+        setCursor(new_cursor)
+        setGettingMessages(false)
+        ScrollContainer.scrollTo({ top: current_scroll, behavior: 'smooth' })
+      })
+      .catch(err => {
+        toast.error('ERROR: Could not load messages.')
+      })
+  }, [GettingMessages]) // ignore the dependency array warning
 
   const submitMessage = async (values: FormValues) => {
     if (values.message!.trim() === '') return
@@ -39,9 +59,14 @@ const Chat: NextPage<Props> = ({ user, messages, cursor }) => {
     reset()
   }
 
+  const handleScroll = (e: React.UIEvent<HTMLDivElement, UIEvent>) => {
+    const scrollable_height = Number((e.target as any).scrollHeight) - Number((e.target as any).clientHeight)
+    if (Number(scrollable_height) - Number(-(e.target as any).scrollTop) < 600 && !GettingMessages && !LoadedAllMessages) setGettingMessages(true)
+  }
+
   return (
     <main className={sheet.container}>
-      <div className={sheet.chat_scroll_container}>
+      <div id="messages_scroller" onScroll={handleScroll} className={sheet.chat_scroll_container}>
         <ul className={sheet.chat_container}>
           {
             Messages.map( message => (
@@ -67,7 +92,7 @@ export const getServerSideProps: GetServerSideProps<Props | {}> = async (ctx: Ge
   const user = await getSession(ctx.req, ctx.res)
   if (!user) return { redirect: { destination: '/', permanent: false } }
   const messages = await getMessagesFromDB(0)
-  return { props: makeSerializable({ user, messages, cursor: 0 }) }
+  return { props: makeSerializable({ user, messages, cursor: messages[ 0 ].id }) }
 }
 
 export default Chat
